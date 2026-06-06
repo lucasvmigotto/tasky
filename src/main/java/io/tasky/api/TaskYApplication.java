90f0da580426f8b4358b899439739906a16865fb
Lucas <lvmigotto1@gmail.com>
2026-06-06 18:24:38 -0300

    feat: add spring boot application entry point

---
+++ src/main/java/io/tasky/api/TaskYApplication.java
@@ -0,0 +1,14 @@
+package io.tasky.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TaskYApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskYApplication.class, args);
+    }
+}
